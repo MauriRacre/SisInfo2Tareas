aa14a34f168981c16062e7dfa15ad48c016fd8a3
MauriRacre <114952708+MauriRacre@users.noreply.github.com>
2022-10-18 23:19:22 -0400

    Add files via upload

---
+++ InicioDeSesion/src/iniciodesesion/InicioDeSesion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package iniciodesesion;
+
+/**
+ *
+ * @author Familia RC
+ */
+public class InicioDeSesion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InicioSesion abrir=new InicioSesion();
+        abrir.setVisible(true);
+        abrir.setLocationRelativeTo(null);
+    }
+    
+}
